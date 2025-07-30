63520be630d371f202c81893097303542b315181
BeeBarry <barry.namdari@gmail.com>
2025-07-30 20:07:47 +0200

    test to trigger fix in ci.yml file

--- Github_Actions_labs/golang_app_actions/main.go
+++ Github_Actions_labs/golang_app_actions/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello friends, Today Im learning Golang and its super exciting! Looking forward to expand on this knowledge and build some cool Golang apps!")
+	fmt.Println("Hello friends, Today Im learning Golang and its super exciting! Looking forward to expand on this knowledge and build some cool Golang apps!!")
 }
